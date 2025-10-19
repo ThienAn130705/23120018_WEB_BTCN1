38120d08faf0481d47e84b8cf1a776802bbee6d3
23120018 <23120018@student.hcmus.edu.vn>
2025-10-19 19:35:25 +0700

    sửa style vách ngăn ở phần Footer và thêm code-behind để đồng bộ với phần Nav

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-$("nav button").click(function () {
-  $("nav button").removeClass("active")
-  $(this).addClass("active")
+$(document).ready(function () {
+  // Khi nhấn vào button có data-menu
+  $("button[data-menu]").on("click", function () {
+    const menu = $(this).data("menu") // lấy tên menu
+
+    // Bỏ class active button trong nav và footer
+    $("nav button, footer button").removeClass("active")
+
+    // Thêm class active cho button được chọn có cùng data-menu trong nav và footer
+    $(`button[data-menu="${menu}"]`).addClass("active")
+  })
 })
